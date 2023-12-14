519dbe1daac368f3444ae7cefd269ba79fffe2e6
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2023-12-14 23:43:02 +0000

    Add Authority model

---
+++ src/Domain/Entities/Authority.cs
@@ -0,0 +1,35 @@
+﻿using Rootwire.Hilltop.Domain.Common;
+using Rootwire.Hilltop.Domain.Extensions;
+
+namespace Rootwire.Hilltop.Domain.Entities;
+
+public class Authority : BaseAuditableEntity
+{
+    public const string _prefix = "authority";
+
+    private string _name;
+    private string _description;
+        
+    /// <summary>
+    /// Authority Name
+    /// </summary>
+    public string Name { get => _name;
+        set { _name = value; }
+    }
+    
+    /// <summary>
+    /// More detailed description of the authority
+    /// </summary>
+    public string? Description { get => _description;
+        set { _description = value; }
+    }
+    
+    public Authority(string name, string description = null)
+    {
+        Name = name;
+        Description = description;
+        
+        string normalisedName = Name.GenerateEntityIdentifier();
+        Identifier = $"{_prefix}:{normalisedName}";
+    }
+}
---
+++ test/Domain.Test/Entities/AuthorityTests.cs
@@ -0,0 +1,17 @@
+﻿using Rootwire.Hilltop.Domain.Entities;
+
+namespace Rootwire.Hilltop.Domain.Test.Entities;
+
+public class AuthorityTests
+{
+    [Theory]
+    [InlineData("Cyberessentials", "authority:cyberessentials")]
+    [InlineData("Long Company Name 35", "authority:long-company-name-35")]
+    public void Identifier_IsGenerated_Correctly(string input, string expectedOutput)
+    {
+        Authority _sut = new Authority(name: input);
+        Assert.NotNull(_sut.Identifier);
+        Assert.Equal(expectedOutput, _sut.Identifier);
+    }
+    
+}
