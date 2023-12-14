9f6c42be4b7249310f905b22960a8c5aed9a291b
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2023-12-14 23:52:11 +0000

    Add ReferenceResource

---
+++ src/Domain/Entities/ReferenceResource.cs
@@ -0,0 +1,20 @@
+﻿using Rootwire.Hilltop.Domain.Common;
+
+namespace Rootwire.Hilltop.Domain.Entities;
+
+public class ReferenceResource : BaseAuditableEntity
+{
+    /// <summary>
+    /// The location where the resource can be found.
+    /// </summary>
+    public Uri Uri { get; set; }
+    /// <summary>
+    /// The resource version reference number, if present
+    /// </summary>
+    public string? Version { get; set; }
+    /// <summary>
+    /// The resource release date, if known
+    /// </summary>
+    public DateOnly? ReleaseDate { get; set; }
+    
+}
