b88b4f219296fb69818ac1d761e35b8f5199db7c
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2023-12-14 23:46:03 +0000

    Add BaseEntity and BaseAuditableEntity

---
+++ src/Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Rootwire.Hilltop.Domain.Common;
+
+public abstract class BaseAuditableEntity : BaseEntity
+{
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    public string? CreatedBy { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set; }
+}
---
+++ src/Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Rootwire.Hilltop.Domain.Common;
+
+public abstract class BaseEntity
+{
+    /// <summary>
+    /// Hyphen seperated identifier
+    /// </summary>
+    public string Identifier { get; set; }
+
+    /// <summary>
+    /// Database Primary Key
+    /// </summary>
+    public Guid Id { get; set; } = Guid.NewGuid();
+}
