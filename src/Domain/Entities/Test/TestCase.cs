eb0347a130bd8b085bfe0f541bfc340b22932994
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2023-12-15 00:45:53 +0000

    Add TestCase and TestCaseTests

---
+++ src/Domain/Entities/Test/TestCase.cs
@@ -0,0 +1,45 @@
+﻿using Rootwire.Hilltop.Domain.Common;
+using Rootwire.Hilltop.Domain.Extensions;
+
+namespace Rootwire.Hilltop.Domain.Entities.Test;
+
+public class TestCase : BaseAuditableEntity
+{
+    public const string _prefix = "testcase";
+    
+    private string _identifier;
+    
+    /// <summary>
+    /// A String based, human readable identifier
+    /// </summary>
+    public string Identifier
+    {
+        get => _identifier;
+        set => _identifier = value.GenerateEntityIdentifier();
+    }
+    
+    public string ReferenceTag
+    {
+        get => $"{_prefix}:{_identifier}";
+    }
+    
+    /// <summary>
+    /// Summary of the Test Case
+    /// </summary>
+    public string Summary { get; set; }
+    
+    /// <summary>
+    /// More detailed description of the test case
+    /// </summary>
+    public string Description { get; set; }
+
+    public Aggregate Aggregate { get; set; }
+    
+    public Category Category { get; set; }
+
+    public void SetIdentifier()
+    {
+        string normalisedSummary = Summary.GenerateEntityIdentifier();
+        Identifier = $"{_prefix}_{normalisedSummary}";
+    }
+}
---
+++ test/Domain.Test/Entities/Test/TestCaseTests.cs
@@ -0,0 +1,32 @@
+﻿using Rootwire.Hilltop.Domain.Entities.Test;
+
+namespace Rootwire.Hilltop.Domain.Test.Entities.Test;
+
+public class TestCaseTests
+{
+    [Theory]
+    [InlineData("1.2.1", "1.2.1")]
+    [InlineData("Authentication required to access service", "authentication-required-to-access-service")]
+    public void Identifier_Is_Generated_Correctly(string input, string expectedOutput)
+    {
+        TestCase _sut = new TestCase
+        {
+            Identifier = input
+        };
+        Assert.NotNull(_sut.Identifier);
+        Assert.Equal(expectedOutput, _sut.Identifier);
+    }
+    
+    [Theory]
+    [InlineData("1.2.1", "testcase:1.2.1")]
+    [InlineData("Authentication required to access service", "testcase:authentication-required-to-access-service")]
+    public void ReferenceTag_Is_Generated_Correctly(string input, string expectedOutput)
+    {
+        TestCase _sut = new TestCase
+        {
+            Identifier = input
+        };
+        Assert.NotNull(_sut.ReferenceTag);
+        Assert.Equal(expectedOutput, _sut.ReferenceTag);
+    }
+}
