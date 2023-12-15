8ea2792a1bd0c71517e67a5878e22b8cccf7186e
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2023-12-15 00:07:25 +0000

    Add Specification and SpecificationTests

---
+++ src/Domain/Entities/Test/Specification.cs
@@ -0,0 +1,35 @@
+﻿using Rootwire.Hilltop.Domain.Common;
+using Rootwire.Hilltop.Domain.Extensions;
+
+namespace Rootwire.Hilltop.Domain.Entities.Test;
+
+public class Specification : BaseAuditableEntity
+{
+    public const string _prefix = "spec";
+
+    private string _identifier;
+    
+    /// <summary>
+    /// The Name of the Specification
+    /// </summary>
+    public string Name { get; set; }
+    
+    /// <summary>
+    /// Hyphen seperated identifier
+    /// </summary>
+    public string Identifier
+    {
+        get => _identifier;
+        set => _identifier = $"{_prefix}:{value.GenerateEntityIdentifier()}";
+    }
+
+    /// <summary>
+    /// The Authority responsible for maintaining the specification
+    /// </summary>
+    public Authority Authority { get; set; }
+    
+    /// <summary>
+    /// The Specification Reference Material
+    /// </summary>
+    public ReferenceResource Reference { get; set; }
+}
---
+++ test/Domain.Test/Entities/Test/SpecificationTests.cs
@@ -0,0 +1,19 @@
+﻿using Rootwire.Hilltop.Domain.Entities.Test;
+
+namespace Rootwire.Hilltop.Domain.Test.Entities.Test;
+
+public class SpecificationTests
+{
+    [Theory]
+    [InlineData("2023 10 31", "spec:2023-10-31")]
+    [InlineData("Cyber-Essentials-Plus-Illustrative-Technical-Specification-v3-1-January-2023.pdf", "spec:cyber-essentials-plus-illustrative-technical-specification-v3-1-january-2023.pdf")]
+    public void Identifier_IsGenerated_Correctly(string input, string expectedOutput)
+    {
+        Specification _sut = new Specification
+        {
+            Identifier = input
+        };
+        Assert.NotNull(_sut.Identifier);
+        Assert.Equal(expectedOutput, _sut.Identifier);
+    }
+}
