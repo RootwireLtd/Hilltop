f2ceacf9f5cf18f94df15309075c0712928abbcb
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2023-12-15 01:38:17 +0000

    Add LogEntry and LogEntryTests

---
+++ src/Domain/Entities/Test/LogEntry.cs
@@ -0,0 +1,32 @@
+﻿using Rootwire.Hilltop.Domain.Common;
+
+namespace Rootwire.Hilltop.Domain.Entities.Test;
+
+public class LogEntry : BaseAuditableEntity
+{
+    private const string _prefix = "testlog";
+    
+    private string _assessmentDescriptor;
+    public TestCase TestCase { get; set; }
+    public bool IsSuccess { get; set; }
+    public string TestResultDetail { get; set; }
+
+    public string AssessmentDescriptor
+    {
+        get => _assessmentDescriptor;
+        set { SetAssessmentDescriptor(); }
+    }
+
+    public void SetAssessmentDescriptor()
+    {
+        var authorityReferenceTag = TestCase.Aggregate.Specification.Authority.ReferenceTag;
+        var specificationReferenceTag = TestCase.Aggregate.Specification.ReferenceTag;
+        var aggregateReferenceTag = TestCase.Aggregate.ReferenceTag;
+        var testcaseReferenceTag = TestCase.ReferenceTag;
+        var resultText = IsSuccess ? "success-result" : "failed-result";
+        
+        string assessmentDescriptor = $"{authorityReferenceTag} > {specificationReferenceTag} > {aggregateReferenceTag} > {testcaseReferenceTag} > {_prefix}:{resultText}";
+
+        _assessmentDescriptor = assessmentDescriptor;
+    }
+}
---
+++ test/Domain.Test/Entities/Test/LogEntryTests.cs
@@ -0,0 +1,47 @@
+﻿using Rootwire.Hilltop.Domain.Entities;
+using Rootwire.Hilltop.Domain.Entities.Test;
+
+namespace Rootwire.Hilltop.Domain.Test.Entities.Test;
+
+public class LogEntryTests
+{
+    [Fact]
+    public void GetAssessmentDescriptor_Generates_Correct_Result()
+    {
+        Authority authority = new Authority()
+        {
+            Identifier = "CyberEssentials"
+        };
+        
+
+        Specification specification = new Specification()
+        {
+            Authority = authority,
+            Identifier = "Cyber-Essentials-Plus-Illustrative-Technical-Specification-v3-1-January-2023"
+        };
+
+        Aggregate aggregate = new Aggregate()
+        {
+            Specification = specification,
+            Identifier = "Remote Vulnerability Assessment"
+        };
+
+        TestCase testCase = new TestCase()
+        {
+            Aggregate = aggregate,
+            Identifier = "Authentication required to access service"
+        };
+
+        LogEntry logEntry = new LogEntry()
+        {
+            TestCase = testCase,
+            IsSuccess = true
+        };
+
+        logEntry.SetAssessmentDescriptor();
+        
+        Assert.NotNull(logEntry.AssessmentDescriptor);;
+        string expectedOutput = "authority:cyberessentials > spec:cyber-essentials-plus-illustrative-technical-specification-v3-1-january-2023 > aggregate:remote-vulnerability-assessment > testcase:authentication-required-to-access-service > testlog:success-result";
+        Assert.Equal(expectedOutput,logEntry.AssessmentDescriptor);
+    }
+}
