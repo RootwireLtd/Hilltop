24b99e75def5de792a70c8cd76680cf7066d5d63
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2023-12-15 00:42:34 +0000

    Add Category entity

---
+++ src/Domain/Entities/Test/Category.cs
@@ -0,0 +1,23 @@
+﻿using Rootwire.Hilltop.Domain.Common;
+
+namespace Rootwire.Hilltop.Domain.Entities.Test;
+
+public class Category : BaseAuditableEntity
+{
+    /// <summary>
+    /// The name of the Category.
+    /// EG: Firewalls
+    /// </summary>
+    public string Name { get; set; }
+    
+    /// <summary>
+    /// Additional information about the category
+    /// </summary>
+    public string Description { get; set; }
+    
+    /// <summary>
+    /// The Parent Category
+    /// EG: CyberSecurity
+    /// </summary>
+    public Category ParentCategory { get; set; }
+}
