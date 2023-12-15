1e15af9fe073974d05331a90dfa51cf678af2ea4
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2023-12-15 00:37:41 +0000

    Add Aggregate and AggregateTests

---
+++ src/Domain/Entities/Test/Aggregate.cs
@@ -0,0 +1,46 @@
+﻿using Rootwire.Hilltop.Domain.Common;
+using Rootwire.Hilltop.Domain.Extensions;
+
+namespace Rootwire.Hilltop.Domain.Entities.Test;
+
+public class Aggregate : BaseAuditableEntity
+{
+    public const string _prefix = "aggregate";
+    
+    private string _identifier;
+    
+    /// <summary>
+    /// A String based identifier
+    /// </summary>
+    public string Identifier
+    {
+        get => _identifier;
+        set => _identifier = value.GenerateEntityIdentifier();
+    }
+    
+    public string ReferenceTag
+    {
+        get => $"{_prefix}:{_identifier}";
+    }
+    
+    /// <summary>
+    /// Summary of the Aggregate
+    /// </summary>
+    public string Summary { get; set; }
+
+    /// <summary>
+    /// More detailed description of the aggregate
+    /// </summary>
+    public string? Description { get; set; }
+    
+    /// <summary>
+    /// Specification the Aggregate belongs to
+    /// </summary>
+    public Specification Specification { get; set; }
+
+    /// <summary>
+    /// The Parent Aggregate
+    /// </summary>
+    public Aggregate? Parent { get; set; }
+    
+}
---
+++ test/Domain.Test/Entities/Test/AggregateTests.cs
@@ -0,0 +1,34 @@
+﻿using Rootwire.Hilltop.Domain.Entities.Test;
+
+namespace Rootwire.Hilltop.Domain.Test.Entities.Test;
+
+public class AggregateTests
+{
+    [Theory]
+    [InlineData("Remote Vulnerability Assessment", "remote-vulnerability-assessment")]
+    [InlineData("Check patching, by authenticated vuln scan", "check-patching-by-authenticated-vuln-scan")]
+    [InlineData("Check malware protection", "check-malware-protection")]
+    public void Identifier_Is_Generated_Correctly(string input, string expectedOutput)
+    {
+        Aggregate _sut = new Aggregate
+        {
+            Identifier = input
+        };
+        Assert.NotNull(_sut.Identifier);
+        Assert.Equal(expectedOutput, _sut.Identifier);
+    }
+    
+    [Theory]
+    [InlineData("Remote Vulnerability Assessment", "aggregate:remote-vulnerability-assessment")]
+    [InlineData("Check patching, by authenticated vuln scan", "aggregate:check-patching-by-authenticated-vuln-scan")]
+    [InlineData("Check malware protection", "aggregate:check-malware-protection")]
+    public void ReferenceTag_Is_Generated_Correctly(string input, string expectedOutput)
+    {
+        Aggregate _sut = new Aggregate
+        {
+            Identifier = input
+        };
+        Assert.NotNull(_sut.ReferenceTag);
+        Assert.Equal(expectedOutput, _sut.ReferenceTag);
+    }
+}
